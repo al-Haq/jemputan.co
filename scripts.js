41c599a221be7e9ee51d8c8be26561531f510923
al-haq <w.amirulnajmi@gmail.com>
2023-04-08 00:20:37 +0800

    Updated

--- scripts.js
+++ scripts.js
@@ -2,34 +2,57 @@ const products = [
     {
         id: 1,
         name: 'Product 1',
-        image: 'https://via.placeholder.com/200',
+        image: 'images/1.jpg',
         price: 19.99
     },
     {
         id: 2,
         name: 'Product 2',
-        image: 'https://via.placeholder.com/200',
+        image: 'images/2.jpg',
         price: 29.99
     },
     {
         id: 3,
         name: 'Product 3',
-        image: 'https://via.placeholder.com/200',
+        image: 'images/3.jpg',
         price: 39.99
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        image: 'images/4.jpg',
+        price: 49.99
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        image: 'https://via.placeholder.com/200',
+        price: 59.99
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        image: 'https://via.placeholder.com/200',
+        price: 69.99
     }
 ];
 
-const productsContainer = document.querySelector('.products');
+document.addEventListener('DOMContentLoaded', () => {
+    const productsContainer = document.querySelector('.products');
 
-products.forEach(product => {
-    const productDiv = document.createElement('div');
-    productDiv.className = 'product';
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
 
-    productDiv.innerHTML = `
-        <h3>${product.name}</h3>
-        <img src="${product.image}" alt="${product.name}">
-        <p>Price: $${product.price.toFixed(2)}</p>
-    `;
+        productDiv.innerHTML = `
+            <h3>${product.name}</h3>
+            <img src="${product.image}" alt="${product.name}">
+            <div class="preview-container">
+                <img src="${product.image}" alt="${product.name} Preview">
+            </div>
+            <p>Price: MYR ${product.price.toFixed(2)}</p>
+        `;
 
-    productsContainer.appendChild(productDiv);
+        productsContainer.appendChild(productDiv);
+    });
 });
