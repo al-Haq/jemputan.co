74d169951d8f79fd9232d3b95c4890793cdf5c1c
al-haq <w.amirulnajmi@gmail.com>
2023-04-07 16:26:22 +0800

    Version 1

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        image: 'https://via.placeholder.com/200',
+        price: 19.99
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        image: 'https://via.placeholder.com/200',
+        price: 29.99
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        image: 'https://via.placeholder.com/200',
+        price: 39.99
+    }
+];
+
+const productsContainer = document.querySelector('.products');
+
+products.forEach(product => {
+    const productDiv = document.createElement('div');
+    productDiv.className = 'product';
+
+    productDiv.innerHTML = `
+        <h3>${product.name}</h3>
+        <img src="${product.image}" alt="${product.name}">
+        <p>Price: $${product.price.toFixed(2)}</p>
+    `;
+
+    productsContainer.appendChild(productDiv);
+});
